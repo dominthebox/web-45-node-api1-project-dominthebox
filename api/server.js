8a7869b2189139ecbf37c14542a0e515db908d6f
dominthebox <dsallustro@gmail.com>
2021-08-30 16:41:37 -0400

    first commit, dependencies installed, wip

--- api/server.js
+++ api/server.js
@@ -1,3 +1,10 @@
 // BUILD YOUR SERVER HERE
+const express = require('express')
+const server = express()
+const User = require('./users/model')
+server.use(express.json())
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+
+
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
